ce419afeca46054d435b3219145a6866d55584a0
johlbaum <julien.ohlbaum@gmail.com>
2024-10-10 15:15:58 +0200

    Update advice route

--- src/Controller/AdviceController.php
+++ src/Controller/AdviceController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class AdviceController extends AbstractController
@@ -84,5 +85,35 @@ class AdviceController extends AbstractController
         $advice = $serializer->serialize($advice, 'json', ['groups' => 'getAdvice']);
 
         return new JsonResponse($advice, Response::HTTP_CREATED, [], true);
+    }
+
+    /**
+     * Permet de mettre à jour un conseil. 
+     */
+    #[Route('/api/conseil/{id}', name: 'app_updateAdvice', methods: ['PUT'])]
+    public function updateAdvice($id, AdviceRepository $adviceRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, MonthRepository $monthRepository): JsonResponse
+    {
+        $currentAdvice = $adviceRepository->find($id);
+        $jsonAdvice = $request->getContent();
+
+        $advice = $serializer->deserialize($jsonAdvice, Advice::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentAdvice]);
+
+        $content = $request->toArray();
+        $months = $content['month'] ?? [];
+
+        foreach ($months as $monthNumber) {
+            $month = $monthRepository->find($monthNumber);
+
+            if ($month) {
+                $advice->addMonth($month);
+            } else {
+                return new JsonResponse(['error' => 'Invalid month: ' . $monthNumber], Response::HTTP_NOT_FOUND);
+            }
+        }
+
+        $entityManager->persist($advice);
+        $entityManager->flush();
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
