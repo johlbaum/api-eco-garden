26cc70074dcbfeb17ee72461dc85ffb6f547a9f5
johlbaum <julien.ohlbaum@gmail.com>
2024-10-10 14:40:04 +0200

    Get advices by current month route

--- src/Controller/AdviceController.php
+++ src/Controller/AdviceController.php
@@ -29,4 +29,24 @@ class AdviceController extends AbstractController
 
         return new JsonResponse($jsonAdvices, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Permet de récupérer un tableau avec tous les conseils du mois en cours.
+     */
+    #[Route('/api/conseil/', name: 'app_get_advice_by_current_month', methods: 'GET')]
+    public function getAdviceByCurrentMonth(MonthRepository $monthRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $currentMonth = (new \DateTime())->format('n');
+        $month = $monthRepository->find($currentMonth);
+
+        $advices = $month->getAdviceList();
+
+        if ($advices->isEmpty()) {
+            return new JsonResponse(['error' => 'Advices not found.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $jsonAdvices = $serializer->serialize($advices, 'json', ['groups' => 'getAdvice']);
+
+        return new JsonResponse($jsonAdvices, Response::HTTP_OK, [], true);
+    }
 }
